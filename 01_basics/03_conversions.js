4046a9160619cb0124638f03fca99a10340b9931
Aniketcoderr <aniket20dec@gmail.com>
2024-10-28 14:16:07 +0000

    ..

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -24,3 +24,19 @@ let stringnumber =String(somenumber)
 console.log(somenumber)//will give o/p as 33
 console.log(stringnumber)//will give o/p as 33
 console.log( typeof stringnumber)//o/p=string
+
+//*****************Operations*******************************// 
+let value=3
+let Negvalue=-value
+console.log(Negvalue);//o/p=-3
+console.log(2**4)//gives ans as 16 (it is 2 raiset the power 4)
+console.log(2%3)//2
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2+ 2);//122
+console.log(1+ 2 + "2");//32
+//the above 4 are not a goood practice 
+let num1, num2 ,num3 
+num1=num2=num3=2+2
+console.log(num1 ," ",num2," ",num3)//will  give ans  4   4   4 but this is also not a good practice
+
