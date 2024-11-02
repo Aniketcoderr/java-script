c693a7f8431c7888726e29f0b034aed3307fbfb6
Aniketcoderr <aniket20dec@gmail.com>
2024-11-02 13:49:20 +0000

    summary datatyprs

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+/*#primitive(call by value):-orginal data ka reference memory me nahi diya jata inko alag  se copy krke diya jata hai jisse j changes hote hai wooh cpy me hee hote hai while copying.
+1)String
+2)number
+3)boolean
+4)null
+5)undefined
+6)Symbol:-to make a value as unique.(eg. const id =Symbol('123')
+7)bigInt
+
+
+//non primitivve (Reference type):-ye values wo hai jinka reference memoory me directly humko allocate kiya jata hai .
+(*)Arrays, Objects,Functions.*/
+//Objects
+
+ /*let my_obj=   {name:"aniket",
+    age:20
+}
+console.log(my_obj)*/
+//function stored in a variablee xamples-->const myfunction=function(){}
