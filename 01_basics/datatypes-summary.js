0b7d6ee713de679b82033be3fb3c43994494bff9
Aniketcoderr <aniket20dec@gmail.com>
2024-11-02 19:44:43 +0000

    summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,4 +1,4 @@
-/*#primitive(call by value):-orginal data ka reference memory me nahi diya jata inko alag  se copy krke diya jata hai jisse j changes hote hai wooh cpy me hee hote hai while copying.
+/*#primitive(call by value):-orginal data ka reference memory me nahi diya jata inko alag  se copy krke diya jata hai jisse j changes hote hai wooh copy me hee hote hai while copying.
 1)String
 2)number
 3)boolean
@@ -17,3 +17,30 @@
 }
 console.log(my_obj)*/
 //function stored in a variablee xamples-->const myfunction=function(){}
+
+
+//-------------------------++++++++++++++++++++++++++++++----------------
+// memory =1)stack(Primituve) 2)Heap(non-Pimitive)
+/*For primitives the copy is given in the memory 
+ let myName="Aniket"
+ let myAnothername=myName
+  myAnothername="papa"
+  console.log(myAnothername)//papa
+  console.log(myName)//Aniket
+  */
+
+ /*now,lets take an oobject (non-primitive)
+  let user1={
+    email:"1234@gmail.com",
+    upi:"user@ybl"
+
+  }
+  let user2=user1//here the referrence is given itself not the copy
+  user2.email="aniket@gmail.com"
+  console.log(user1)
+
+  console.log(user2)
+  output:- { email: 'aniket@gmail.com', upi: 'user@ybl' }
+  { email: 'aniket@gmail.com', upi: 'user@ybl' }
+  email of both has been Changed as the direct reference is given in the memory 
+  */
